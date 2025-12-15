ac65f10126b10eb408ecc41aadb01dddc9175891
KEXIN LI <danielleeprince@gmail.com>
2025-12-15 11:52:50 +0800

    Add backend/pkg/response/response.go

---
+++ backend/pkg/response/response.go
@@ -0,0 +1,20 @@
+package response
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Success(c *gin.Context, data interface{}) {
+	c.JSON(200, gin.H{
+		"success": true,
+		"data":    data,
+	})
+}
+
+func Error(c *gin.Context, code int, message string) {
+	c.JSON(code, gin.H{
+		"success": false,
+		"error":   message,
+	})
+}
+
